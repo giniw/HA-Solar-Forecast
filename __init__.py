bf101705a6c59745577446f671122327275a4bac
Monoswini Dey <110888282+mandycodez@users.noreply.github.com>
2026-06-25 23:10:49 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
-from homeassistant.core import HomeAssistant
+"""The Solar-Forecast.com integration entry point."""
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
 from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -7,6 +9,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data["api_key"]
 
-    # Forward the setup to the sensor platform
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
